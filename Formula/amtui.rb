f3dcd354104f46589922d3f89b129a156a0a1507
Furkan Pehlivan <furkanpehlivan34@gmail.com>
2023-09-02 18:28:07 +0300

    fix sha256 codes for amtui formula.

--- Formula/amtui.rb
+++ Formula/amtui.rb
@@ -10,7 +10,7 @@ class Amtui < Formula
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/pehlicd/amtui/releases/download/v0.0.2/amtui_v0.0.2_darwin_arm64.tar.gz", using: CurlDownloadStrategy
-        sha256 "b88674364950c116d93946ac8c1372fb56eefdb41d2287dd9adf6e10e8f4bc6e"
+        sha256 "ed429d344555afe36c68233fb33d560bc98017e0dc57a6f369b449a58c36ba6d"
         
         def install
           bin.install "amtui"
@@ -18,7 +18,7 @@ class Amtui < Formula
     end
     if Hardware::CPU.intel?
         url "https://github.com/pehlicd/amtui/releases/download/v0.0.2/amtui_v0.0.2_darwin_amd64.tar.gz", using: CurlDownloadStrategy
-        sha256 "04cd0cbbdc9de598a621faf858aa3046b41ec40c10c179973eb379dec4824bcb"
+        sha256 "a5a0d00df38f777625574e1bc2c3aac954e5b1bdc2c76cbe688bb359213e1a11"
         
         def install
           bin.install "amtui"
@@ -29,7 +29,7 @@ class Amtui < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/pehlicd/amtui/releases/download/v0.0.2/amtui_v0.0.2_linux_arm64.tar.gz", using: CurlDownloadStrategy
-      sha256 "6798af16ed7603ac27327996906fe32092ac418cfc03da7a261bde9d203eb936"
+      sha256 "1131fbfaae9a1564903d151da97a8074fd076c0b18a70858249561533228df02"
 
       def install
         bin.install "amtui"
@@ -37,7 +37,7 @@ class Amtui < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/pehlicd/amtui/releases/download/v0.0.2/amtui_v0.0.2_linux_amd64.tar.gz", using: CurlDownloadStrategy
-      sha256 "c1a23f6e5771c96c0a04143b7b63b51d8a408f40d4b5bcdd33b9732216e77e5d"
+      sha256 "3d28bde1bb630dd8b7d0a57ee0083e0492c3ef268b86eddddd5dd93fb24c7395"
 
       def install
         bin.install "amtui"
