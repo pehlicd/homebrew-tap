52e85795d60c7888d4c3af00ffdfc86f3fd25fee
Furkan Pehlivan <furkanpehlivan34@gmail.com>
2023-09-10 16:21:34 +0300

    add new version of amtui.

--- Formula/amtui.rb
+++ Formula/amtui.rb
@@ -2,8 +2,8 @@ class Amtui < Formula
   desc "Alertmanager TUI - Your Terminal Companion for Alertmanager"
   homepage "https://github.com/pehlicd/amtui"
   url "https://github.com/pehlicd/amtui.git",
-      tag:      "v0.0.3",
-      revision: "abb4e42a755d2fa83d4e8c19cb37bba8a08a7cfc"
+      tag:      "v0.1.1",
+      revision: "c3741c89e2dc3c3b93920e35aa39ce44998e9c75"
   license "MIT"
   head "https://github.com/pehlicd/amtui.git", branch: "main"
 
